a94e0558afdb6ab6a9567a3c756774cecbb9205d
nouveau <11975230+nouveau-rgb@users.noreply.github.com>
2022-07-24 15:24:25 +0800

    ..

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -57,12 +57,13 @@ int main() {
     for (int i = image_height - 1; i >= 0; i--) {
         for (int j = 0; j < image_width; j++) {
             color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width - 1);
-                auto v = (j + random_double()) / (image_height - 1);
+            for (int s = 0; s < samples_per_pixel; s++) {
+                auto u = (double(j) + random_double()) / (image_width - 1);
+                auto v = (double(i) + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world);
             }
+            
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
